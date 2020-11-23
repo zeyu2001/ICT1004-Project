a22d504ac2285ff2e91dbebfbdf66e858582b895
Zeyu <zhangzeyu2001@gmail.com>
2020-11-24 00:12:03 +0800

    process login

--- login_form.php
+++ login_form.php
@@ -1,11 +1,20 @@
-<form action="process_login.php">
+<form action="process_login.php" method="post">
+    <div class="form-group form-row">
+        <div class="col">
+            <label for="account_type">Type of Account:</label>
+            <select class="form-control form-control-md" name="account_type" id="account_type">
+                <option value="freelancer">Freelancer</option>
+                <option value="corporate">Corporate</option>
+            </select>
+        </div>
+    </div>
     <div class="form-group">
         <label for="email">Email:</label>
-        <input class="form-control" id="email" type="email" placeholder="e.g. jamesbond@gmail.com">
+        <input class="form-control" id="email" name="email" type="email" placeholder="e.g. jamesbond@gmail.com">
     </div>
     <div class="form-group ">
         <label for="password">Password:</label>
-        <input class="form-control" id="password" type="password" placeholder="••••••">
+        <input class="form-control" id="password" name="password" type="password" placeholder="••••••">
     </div>
     <div class="form-group">
         <button class="btn btn-primary my-2 my-sm-0" type="submit">Submit</button>
--- process_login.php
+++ process_login.php
@@ -1,38 +1,132 @@
 <?php
     session_start();
-    
-    include "db_functions.inc.php";
-    
-    $QUERY_GET_FREELANCER_BY_ID = "SELECT * FROM manyhires_freelancers WHERE freelancer_id=?";
-    
-    $_SESSION['id'] = 1; // Dummy Session
-    $_SESSION['account_type'] = 'Freelancer';
-    
-    list($return_code, $result, $errorMsg) = query_db($QUERY_GET_FREELANCER_BY_ID, array($_SESSION['id']));
-    
-    if (!$return_code === 0)
-    {
-        echo $errorMsg;
-        exit();
-    }
-    else if ($result->num_rows > 0)
-    {
-        // There should only be one row in the result set
-        $row = $result->fetch_assoc();
-        $fname = $row["fname"];
-        $lname = $row["lname"];
-    }
-    
-    if ($fname)
-    {
-        $_SESSION["display_name"] = $fname . " " . $lname;
-    }
-    else
-    {
-        $_SESSION["display_name"] = $lname;
-    }
-            
-    header("location:index.php");
-    exit();
 ?>
 
+<?php
+    include "validate.inc.php";
+    include "db_functions.inc.php";
+    
+    $QUERY_GET_FREELANCER_BY_EMAIL = "SELECT * FROM manyhires_freelancers WHERE email=?";
+    $QUERY_GET_COMPANY_BY_EMAIL = "SELECT * FROM manyhires_companies WHERE email=?";
+    
+    $success = true;
+    $errorMsg = "";
+    
+    $email = validate_input("email", "email", 
+            FILTER_VALIDATE_REGEXP, array("regexp" => email_filter),
+            "Please enter a valid email.", true, true);
+    $pwd = validate_input("password", "password", 
+            FILTER_VALIDATE_REGEXP, array("regexp" => passwords_filter),
+            "8 or more characters required.", true, false);
+    
+    if ($success){
+        switch ($_POST['account_type'])
+        {
+            case "freelancer":
+                list($return_code, $result, $errorMsg) = query_db($QUERY_GET_FREELANCER_BY_EMAIL, array($email));
+                if ($result->num_rows > 0)
+                {
+                    // Note that email field is unique, so should only have 
+                    // one row in the result set.
+                    $row = $result->fetch_assoc();
+
+                    $fname = $row["fname"];
+                    $lname = $row["lname"];
+                    $hashed_pwd = $row["password"];
+                    if ($fname)
+                    {
+                        $full_name = $fname . " " . $lname;
+                    }
+                    else
+                    {
+                        $full_name = $lname;
+                    }
+
+                    // Check if the password matches:
+                    if (!password_verify($_POST["password"], $hashed_pwd))
+                    {
+                        $errorMsg = "Email not found or password doesn't match..."; 
+                        $success = false;
+                    } 
+                    else
+                    {
+                        $_SESSION['id'] = $row['freelancer_id'];
+                        $_SESSION['account_type'] = 'Freelancer';
+                        $_SESSION['display_name'] = $full_name;
+                                
+                        // Login successful
+                        header("location:index.php");
+                        exit();
+                    }
+                }
+                else 
+                {
+                    $errorMsg = "Email not found or password doesn't match...";
+                    $success = false;
+                }
+                break;
+                
+            case "corporate":
+                list($return_code, $result, $errorMsg) = query_db($QUERY_GET_COMPANY_BY_EMAIL, array($email));
+                                if ($result->num_rows > 0)
+                {
+                    // Note that email field is unique, so should only have 
+                    // one row in the result set.
+                    $row = $result->fetch_assoc();
+                    $hashed_pwd = $row["password"];
+
+                    // Check if the password matches:
+                    if (!password_verify($_POST["password"], $hashed_pwd))
+                    {
+                        $errorMsg = "Email not found or password doesn't match..."; 
+                        $success = false;
+                    } 
+                    else
+                    {
+                        $_SESSION['id'] = $row['company_id'];
+                        $_SESSION['account_type'] = 'Company';
+                        $_SESSION['display_name'] = $row[$name];
+                                
+                        // Login successful
+                        header("location:index.php");
+                        exit();
+                    }
+                }
+                else 
+                {
+                    $errorMsg = "Email not found or password doesn't match...";
+                    $success = false;
+                }
+                break;
+            
+            default:
+                $errorMsg = "Please try again later.";
+                
+                $success = false;
+                break;
+        }
+    }
+?> 
+
+<!DOCTYPE html>
+<html lang="en">
+    <?php
+        include "head.inc.php";
+        include "db_functions.inc.php"
+    ?>
+    <body> 
+        <?php
+            include "nav.inc.php"; 
+        ?>
+        <main class="container"> 
+            <h1> Oops! </h1>
+            <h2> Something went wrong. </h2>
+            <p><?php echo $errorMsg ?></p>
+            <a class='red-button' href='index.php'> Return to Home </a>
+        </main>
+        <?php
+            include "footer.inc.php";
+        ?>
+    </body>
+</html>
+                
