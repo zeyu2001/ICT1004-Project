7ed579eb20b37fd6bd1b155a6815d362440cb2d9
Xeonia <72965305+Xeonia@users.noreply.github.com>
2020-11-26 00:56:39 +0800

    Update head.inc.php

--- head.inc.php
+++ head.inc.php
@@ -1,5 +1,6 @@
 <head>
     <title>ManyHires</title>
+    <link rel="shortcut icon" href="images/favicon.ico" type="image/x-icon">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta
       name="description"           
