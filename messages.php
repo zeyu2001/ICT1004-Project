0d99303d25480f9c29d0d09010a71774d1260417
Zeyu <zhangzeyu2001@gmail.com>
2020-12-04 14:30:04 +0800

    more accessibility

--- messages.php
+++ messages.php
@@ -151,6 +151,7 @@
                     <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]). "?id=". $_GET['id']; ?>">
                         <div class="form-group form-row">
                             <div class="col">
+                                <label for="reply" hidden>Your Message</label>
                                 <input class="form-control" id="reply" name="reply" type="text" placeholder="Your message">
                             </div>
                             <button class="btn btn-primary" type="submit">Send</button>
